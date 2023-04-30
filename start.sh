ef6483e2185d6aea74df8a86d1606341f6558491
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2023-04-30 23:33:17 +0200

    travian bot

--- start.sh
+++ start.sh
@@ -6,5 +6,7 @@ gunicorn --bind 0.0.0.0:5000 --workers 4 app:app &
 # attesa per il completamento dell'avvio del server
 sleep 10
 
+pip install -r requirements.txt
+
 # avvio del bot Selenium
-python bot.py
+python app.py
