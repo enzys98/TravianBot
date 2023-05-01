2f0bd3cf0c90d87f0fab7693a44cef3763faab77
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2023-05-01 15:12:38 +0200

    travian bot

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask
 import requests
-from bs4 import BeautifulSoup
-import json
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from fake_useragent import UserAgent
 import time
 import undetected_chromedriver as uc
 from selenium.common.exceptions import *
