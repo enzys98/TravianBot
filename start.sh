21403822f2302c647db97e8b16755edd7a4c6c1c
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2023-05-01 13:04:00 +0200

    travian bot

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-source ./venv/bin/activate
+# attivazione dell'ambiente virtuale
+source .venv/bin/activate
+
+# installazione delle dipendenze
+pip install -r requirements.txt
 
 # avvio del server Gunicorn con 4 worker process
 gunicorn --bind 0.0.0.0:5000 --workers 4 app:app &
-
-# attesa per il completamento dell'avvio del server
 sleep 10
-
-pip install -r requirements.txt
 
 # avvio del bot Selenium
 python app.py
