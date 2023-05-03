97f1793424e5d0660bb7a29ae659f50315d5e6c8
enzys98 <66466488+enzys98@users.noreply.github.com>
2023-05-03 11:36:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 # attivazione dell'ambiente virtuale
 #source .venv/bin/activate
 export PATH=$PATH:/.chromedriver
-
+export PORT=8080
 # installazione delle dipendenze
 pip install -r requirements.txt
 
 # avvio del server Gunicorn con 4 worker process
+gunicorn app:app
 
 sleep 10
 
 # avvio del bot Selenium
-python app.py
+#python app.py
