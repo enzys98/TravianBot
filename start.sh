ab8cbb0d47159a8c0b8dca9582bca0ee156b130c
enzys98 <66466488+enzys98@users.noreply.github.com>
2023-05-04 17:18:02 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,29 @@
 # attivazione dell'ambiente virtuale
 #source .venv/bin/activate
 
-export PATH=$PATH:/.chromedriver
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
+fi
+
+# be sure to add Chromes location to the PATH as part of your Start Command
+export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+
+# add your own build commands...
 # installazione delle dipendenze
 pip install -r requirements.txt
 
-# avvio del server Gunicorn con 4 worker process
-gunicorn app:app -b 0.0.0.0:$PORT
 
-sleep 10
-
-# avvio del bot Selenium
-#python app.py
